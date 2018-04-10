b264aa6e973e91546d05cbe387b6b584b6b32b9f
wkosea <1254843177@qq.com>
2018-04-10 20:36:14 +0800

    add some example
    重新拉取后试着点击主页的【选课管理】，然后请留意Route和mainController里面的改动，然后提交你的代码。

---
+++ code/app/Http/Controllers/mainController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+class mainController extends Controller{
+	public function XKGL(){
+		return view("XKGL");
+	}
+}
+?>
--- code/resources/views/main.blade.php
+++ code/resources/views/main.blade.php
@@ -44,32 +44,32 @@
         <li class="layui-nav-item layui-nav-itemed">
           <a class="" href="javascript:;">课程管理</a>
           <dl class="layui-nav-child">
-            <dd><a href="javascript:;">选课管理</a></dd>
-            <dd><a href="javascript:;">开课管理</a></dd>
-            <dd><a href="javascript:;">选课统计</a></dd>
-            <dd><a href="javascript:;">成绩查询</a></dd>
-            <dd><a href="javascript:;">成绩统计</a></dd>
-            <dd><a href="javascript:;">成绩录入</a></dd>
+            <dd><a href="javascript:loadView('XKGL');">选课管理</a></dd>
+            <dd><a href="javascript:loadView('KKGL');">开课管理</a></dd>
+            <dd><a href="javascript:loadView('XKTJ');">选课统计</a></dd>
+            <dd><a href="javascript:loadView('CJCX');">成绩查询</a></dd>
+            <dd><a href="javascript:loadView('CJTJ');">成绩统计</a></dd>
+            <dd><a href="javascript:loadView('CJLR');">成绩录入</a></dd>
           </dl>
         </li>
         <li class="layui-nav-item">
           <a href="javascript:;">培养方案</a>
           <dl class="layui-nav-child">
-            <dd><a href="javascript:;">培养方案列表</a></dd>
-            <dd><a href="javascript:;">已选培养方案</a></dd>
-            <dd><a href="javascript:;">创建培养方案</a></dd>
+            <dd><a href="javascript:loadView('PYFA');">培养方案列表</a></dd>
+            <dd><a href="javascript:loadView('YXFA');">已选培养方案</a></dd>
+            <dd><a href="javascript:loadView('CJFA');">创建培养方案</a></dd>
           </dl>
         </li>
-        <li class="layui-nav-item"><a href="">学期设置</a></li>
-        <li class="layui-nav-item"><a href="">教学评估</a></li>
-        <li class="layui-nav-item"><a href="">实验管理</a></li>
-        <li class="layui-nav-item"><a href="">专业调整</a></li>
-        <li class="layui-nav-item"><a href="">毕业管理</a></li>
+        <li class="layui-nav-item"><a href="javascript:loadView('XQSZ');">学期设置</a></li>
+        <li class="layui-nav-item"><a href="javascript:loadView('JXPG');">教学评估</a></li>
+        <li class="layui-nav-item"><a href="javascript:loadView('SYGL');">实验管理</a></li>
+        <li class="layui-nav-item"><a href="javascript:loadView('ZYTZ');">专业调整</a></li>
+        <li class="layui-nav-item"><a href="javascript:loadView('BYGL');">毕业管理</a></li>
       </ul>
     </div>
   </div>
   
-  <div class="layui-body">
+  <div class="layui-body" id="contentView">
     <!-- 内容主体区域 -->
     <div style="padding: 30px;font-size: 40px;">相 关 功 能 模 块</div>
   </div>
@@ -80,11 +80,45 @@
   </div>
 </div>
 <script src="/layui/layui.js"></script>
+<script src="/js/jquery-3.3.1.min.js"></script>>
 <script>
-//JavaScript代码区域
+/*
+  说明
+  在点击左侧的按钮后通过ajax加载相应的视图
+  
+ 【伪代码如下】
+  js部分：
+  $("学期设置").click(funtion(
+    response = ajax("/XQSZ");
+    $("#contentView").html(response);
+  ))
+  路由部分：
+  Route::get("/XQSZ","XQSZCOntroller@f");
+  控制器部分：
+  class XQSZController{
+    public function f(){
+      return view("XQSZ");
+    }
+  }
+  视图部分：
+  自己写相应的view，应该是独立的blade.php文件
+  注意只需要写内容DIV的内容，不是整个html页面。
+*/
 layui.use('element', function(){
   var element = layui.element;
 });
+
+function loadView(viewString){
+  $("#contentView").html("正在加载...");
+  $.get("/" + viewString,function(data,status){
+    if(status == "success")
+      $("#contentView").html(data);
+    else
+      alert("加载失败！");
+  });
+}
+
+
 </script>
 </body>
 </html>
